efcc8e8181a0447a03cc9fdeb37e5a6dbf0236a1
Peter Szasz <xszasz@fi.muni.cz>
2022-04-27 13:10:34 +0200

    last change

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -2,6 +2,7 @@
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import subprocess
 import base64
+import cPickle
 import flask
 
 # Input injection
